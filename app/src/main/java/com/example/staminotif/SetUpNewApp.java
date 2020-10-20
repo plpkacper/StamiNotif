21532307e80bb80ad69418bd9c2e69084523d42b
plpkacper <43370662+plpkacper@users.noreply.github.com>
2020-10-20 18:18:29 +0100

    Initial Design Commit

---
+++ app/src/main/java/com/example/staminotif/SetUpNewApp.java
@@ -0,0 +1,14 @@
+package com.example.staminotif;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetUpNewApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_up_new_app);
+    }
+}
